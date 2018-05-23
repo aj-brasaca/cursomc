5151f227e5bccf91ed3ac35926586b0962ba82d6
Adalberto Brasaca <aj-brasaca@hotmail.com>
2018-05-23 14:38:55 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/brasaca/cursomc/CursomcApplication.java
+++ src/main/java/com/brasaca/cursomc/CursomcApplication.java
@@ -1,22 +1,26 @@
 package com.brasaca.cursomc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.brasaca.cursomc.domain.Categoria;
+import com.brasaca.cursomc.domain.Produto;
 import com.brasaca.cursomc.repositories.CategoriaRepository;
+import com.brasaca.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,16 +28,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		List<Categoria> categorias = new ArrayList<>();
-		
+						
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		categorias.add(cat1);
-		categorias.add(cat2);
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 80.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		categoriaRepository.saveAll(categorias);
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+				
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 }
